3fdb4f1930969c6069c3f9d9771d0980ab4aac92
Suneel Kanuri <suneel.kanuri@gmail.com>
2024-07-01 11:51:32 -0400

    LinkedIn React Agent
    LinkedIn React Agent Implemented

---
+++ tools/tools.py
@@ -0,0 +1,6 @@
+from langchain_community.tools.tavily_search import TavilySearchResults
+def get_profile_url_tavily(name:str):
+    """Searches for Linkedin or Twitter Profile page."""
+    search = TavilySearchResults()
+    res = search.run(f"{name}")
+    return res[0]["url"]
